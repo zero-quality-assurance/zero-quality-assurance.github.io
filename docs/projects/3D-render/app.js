73dc8b160385ba675c3b3c2c647309e2cfd9ae1d
TrytonGolem <trytgol5876@gmail.com>
2023-11-09 21:53:24 +0100

    Animation progress

--- docs/projects/3D-render/app.js
+++ docs/projects/3D-render/app.js
@@ -7,6 +7,7 @@ const frameCount = 180;
 
 const currentFrame = (index) => `./images/${(index + 1).toString()}.jpg`;
 const images = [];
+let ball = { frame : 0 };
 
 for(let i = 0; i < frameCount; i++){
     const img = new Image();
@@ -14,4 +15,22 @@ for(let i = 0; i < frameCount; i++){
     images.push(img);
 }
 
-console.log(images);
+gsap.to(ball, {
+    frame: frameCount - 1,
+    snap: "frame",
+    ease: "none",
+    scrollTrigger: {
+        scrub: true,
+        pin: "canvas",
+        end: "500%",
+    },
+});
+
+
+images[0].onload = render;
+
+
+function render(){
+    context.clearRect(0,0,canvas.width, canvas.height);
+    context.drawImage(images[ball.frame], 0, 0);
+}
